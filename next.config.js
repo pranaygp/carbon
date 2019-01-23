121d97ad3b78c4c1ec25f9932aa19725c87e86cd
raboid <sean@dawnlabs.io>
2019-01-23 12:33:09 -0500

    Research into lodash bundle reduction

--- next.config.js
+++ next.config.js
@@ -1,23 +1,39 @@
 // const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 // const withOffline = require('next-offline')
 
-module.exports = (/* phase  { defaultConfig } */) => {
-  const config = {
-    async exportPathMap() {
-      return {
-        '/about': { page: '/about' },
-        '/embed': { page: '/embed' },
-        '/index': { page: '/index' },
-        '/': { page: '/' }
-      }
-    },
-    publicRuntimeConfig: {
-      API_URL:
-        process.env.NODE_ENV === 'production'
-          ? 'https://carbon-api.now.sh'
-          : 'http://localhost:4000'
-    }
-  }
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
-  return config
-}
+module.exports = withBundleAnalyzer({
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html'
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html'
+    }
+  },
+
+  // async exportPathMap() {
+  //   return {
+  //     '/about': { page: '/about' },
+  //     '/embed': { page: '/embed' },
+  //     '/index': { page: '/index' },
+  //     '/': { page: '/' }
+  //   }
+  // },
+  // publicRuntimeConfig: {
+  //   API_URL:
+  //     process.env.NODE_ENV === 'production' ? 'https://carbon-api.now.sh' : 'http://localhost:4000'
+  // },
+
+  webpack(config) {
+    config.plugins.push(new LodashModuleReplacementPlugin())
+
+    return config
+  }
+})
